6a1f1bed49c507df93bd6157ad8c81ea6c9a292b
Michael Mayer <mjmayer@ucdavis.edu>
2017-08-24 17:33:08 -0700

    bigfix query function added

--- bigfix.go
+++ bigfix.go
@@ -11,13 +11,15 @@ import (
 	"net/http/cookiejar"
 )
 
+//An Computer represents a computer object returend from bigfix
 type Computer struct {
 	LastReport string `xml:"LastReportTime"`
 	ID         string `xml:"ID"`
 }
 
+//An Computers represents the XML returned from bigfix
 type Computers struct {
-	Comp      string     `xml:"BESAPI"`
+	XMLName   string     `xml:"BESAPI"`
 	Computers []Computer `xml:"Computer"`
 }
 
@@ -30,10 +32,11 @@ func main() {
 	)
 	flag.Parse()
 	var session = bigfixlogin(*user, *password, *bigfixurl)
-	println(bigfixcomputers(*bigfixurl, session, *user, *password))
-
+	//println(len(bigfixcomputers(*bigfixurl, session, *user, *password).Computers))
+	bigfixquery(*bigfixurl, session, *user, *password, "names of bes computers")
 }
 
+//Sets up http session for interaction with bigfix.
 func bigfixlogin(user string, password string, bigfixurl string) *http.Client {
 	//allows for invalid certs
 	tr := &http.Transport{
@@ -57,7 +60,8 @@ func bigfixlogin(user string, password string, bigfixurl string) *http.Client {
 	return client
 }
 
-func bigfixcomputers(bigfixurl string, client *http.Client, user string, password string) bool {
+//Returns Computer structure containing computer with lastreporttime, and ID
+func bigfixcomputers(bigfixurl string, client *http.Client, user string, password string) Computers {
 	req, err := http.NewRequest("GET", bigfixurl+"/api/computers", nil)
 	req.SetBasicAuth(user, password)
 	resp, err := client.Do(req)
@@ -73,7 +77,53 @@ func bigfixcomputers(bigfixurl string, client *http.Client, user string, passwor
 	if xml != nil {
 		log.Fatal(err)
 	}
-	//s := string(bodyText)
-	fmt.Println(comps)
-	return (true)
+	return comps
+}
+
+type Result string
+type Answer string
+
+type Eval struct {
+	//XMLName     xml.Name `xml:"Evalutation"`
+	Time        string `xml:"Time"`
+	Pluralality string `xml:"Plurality"`
+}
+
+type Query struct {
+	XMLName    xml.Name `xml:"Query"`
+	Resource   string   `xml:"Resource,attr"`
+	Answers    []Answer `xml:"Result>Answer"`
+	Evaluation []Eval   `xml:"Evalutation`
+}
+
+//An query represents the XML returned from bigfix
+type BESQuery struct {
+	XMLName string `xml:"BESAPI"`
+	Query   Query  `xml:"Query"`
+	//Answers []Answer `xml:"Query>Result>Answer"`
+}
+
+//Runs relevance query against bigfix server
+func bigfixquery(bigfixurl string, client *http.Client, user string, password string, relevance string) BESQuery {
+	req, err := http.NewRequest("GET", bigfixurl+"/api/query", nil)
+	req.SetBasicAuth(user, password)
+	q := req.URL.Query()
+	q.Add("relevance", relevance)
+	req.URL.RawQuery = q.Encode()
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	s := string(bodyText)
+	//println(s)
+	f := []byte(s)
+	var answer BESQuery
+	xml := xml.Unmarshal(f, &answer)
+	if xml != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(answer)
+	return answer
 }
